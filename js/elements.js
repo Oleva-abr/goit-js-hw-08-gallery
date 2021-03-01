fd9149780443b77e9d4f4e36b1ed9540b8db2b16
Olena Abramova <olena.v.abramova@gmail.com>
2021-03-01 21:15:51 +0200

    create gallery

--- js/elements.js
+++ js/elements.js
@@ -1,4 +1,27 @@
 import images from "./gallery-items.js";
 
-const container = document.querySelector('.gallery js-gallery');
+const galleryList = document.querySelector('.js-gallery');
 
+
+
+const createGalleryElements = (({ original, preview, description }) => {
+    return `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `
+});
+
+const imgList = images.map(createGalleryElements).join('');
+console.log(imgList);
+
+galleryList.insertAdjacentHTML('beforeEnd', imgList);

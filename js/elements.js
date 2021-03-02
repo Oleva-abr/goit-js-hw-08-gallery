a83b4f05c70c987d26cc581da0edb731797025f6
Olena Abramova <olena.v.abramova@gmail.com>
2021-03-02 21:58:55 +0200

    add task8

--- js/elements.js
+++ js/elements.js
@@ -1,6 +1,15 @@
 import images from "./gallery-items.js";
 
-const galleryList = document.querySelector('.js-gallery');
+
+const refs = {
+  gallery: document.querySelector('.js-gallery'),
+  lightbox: document.querySelector(".js-lightbox"),
+  btn: document.querySelector('[data-action="close-lightbox"]'),
+  modal: document.querySelector(".lightbox__content"),
+  lightbox__image: document.querySelector(".lightbox__image"),
+  lightbox__overlay: document.querySelector(".lightbox__overlay")
+}
+
 
 
 
@@ -22,6 +31,50 @@ const createGalleryElements = (({ original, preview, description }) => {
 });
 
 const imgList = images.map(createGalleryElements).join('');
-console.log(imgList);
+// console.log(imgList);
 
-galleryList.insertAdjacentHTML('beforeEnd', imgList);
+refs.gallery.insertAdjacentHTML('beforeEnd', imgList);
+
+
+refs.gallery.addEventListener('click', onGalleryListClick)
+
+ 
+
+function onGalleryListClick(event) {
+ 
+  event.preventDefault();
+  
+  if (event.target.nodeName !== 'IMG') {
+    return
+  }
+  else {
+    refs.lightbox.classList.add('is-open');
+    refs.lightbox__image.src = event.target.getAttribute("data-source");
+    refs.lightbox__image.alt = event.target.alt
+        
+    }
+  
+}
+
+  
+refs.btn.addEventListener('click', onBtnClickClose);
+
+function onBtnClickClose(event) { 
+  // event.preventDefault();
+  refs.lightbox.classList.remove("is-open");
+  refs.lightbox__image.src = '';
+  refs.lightbox__image.alt = '';
+  
+}
+
+window.addEventListener('keydown', event => {
+  if (event.code==="Escape") {
+    onBtnClickClose(event)
+  }
+}
+)
+
+refs.lightbox__overlay.addEventListener('click',onBtnClickClose);
+
+
+
